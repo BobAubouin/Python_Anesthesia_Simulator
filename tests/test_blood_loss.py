d47be06031c99ebbf9c37bc6996753028262519a
Bob Aubouin <bob.aubouin-pairault@univ-grenoble-alpes.fr>
2023-03-30 17:15:20 +0200

    add assert to test files

--- tests/test_blood_loss.py
+++ tests/test_blood_loss.py
@@ -20,7 +20,7 @@ George = simulator.Patient([age, height, weight, gender], ts=ts,
 N_simu = int(60 * 60/ts)
 
 
-uP, uR, uN = 0.17, 0.1, 2
+uP, uR, uN = 0.15, 0.1, 2
 George.initialized_at_given_input(u_propo=uP, u_remi=uR, u_nore=uN)
 blood_loss_rate = 200  # ml/min
 blood_gain_rate = 50  # ml/min
@@ -28,53 +28,74 @@ for index in range(N_simu):
     if index > 1*60/ts and index < 11*60/ts:
         Bis, Co, Map, Tol = George.one_step(u_propo=uP, u_remi=uR, u_nore=uN,
                                             blood_rate=- blood_loss_rate, noise=False)
-    if index > 15*60/ts and index < 55*60/ts:
+    elif index > 15*60/ts and index < 55*60/ts:
         Bis, Co, Map, Tol = George.one_step(u_propo=uP, u_remi=uR, u_nore=uN,
                                             blood_rate=blood_gain_rate, noise=False)
     else:
         Bis, Co, Map, Tol = George.one_step(u_propo=uP, u_remi=uR, u_nore=uN,
                                             blood_rate=0, noise=False)
 # %% plot
-fig, ax = plt.subplots(3)
-Time = George.dataframe['Time']/60
-ax[0].plot(Time, George.dataframe['u_propo'])
-ax[1].plot(Time, George.dataframe['u_remi'])
-ax[2].plot(Time, George.dataframe['u_nore'])
+if __name__ == '__main__':
+    fig, ax = plt.subplots(3)
+    Time = George.dataframe['Time']/60
+    ax[0].plot(Time, George.dataframe['u_propo'])
+    ax[1].plot(Time, George.dataframe['u_remi'])
+    ax[2].plot(Time, George.dataframe['u_nore'])
 
-ax[0].set_ylabel("Propo")
-ax[1].set_ylabel("Remi")
-ax[2].set_ylabel("Nore")
-for i in range(3):
-    ax[i].grid()
-plt.ticklabel_format(style='plain')
-plt.show()
+    ax[0].set_ylabel("Propo")
+    ax[1].set_ylabel("Remi")
+    ax[2].set_ylabel("Nore")
+    for i in range(3):
+        ax[i].grid()
+    plt.ticklabel_format(style='plain')
+    plt.show()
 
-fig, ax = plt.subplots(1)
+    fig, ax = plt.subplots(1)
 
-ax.plot(Time, George.dataframe['x_propo_4']*10, label="Propofol (x10)")
-ax.plot(Time, George.dataframe['x_remi_4']*10, label="Remifentanil(x10)")
-ax.plot(Time, George.dataframe['c_blood_nore'], label="Norepinephrine")
-plt.title("Hypnotic effect site Concentration")
-ax.set_xlabel("Time (min)")
-plt.legend()
-plt.grid()
-plt.show()
+    ax.plot(Time, George.dataframe['x_propo_4']*10, label="Propofol (x10)")
+    ax.plot(Time, George.dataframe['x_remi_4']*10, label="Remifentanil(x10)")
+    ax.plot(Time, George.dataframe['c_blood_nore'], label="Norepinephrine")
+    plt.title("Hypnotic effect site Concentration")
+    ax.set_xlabel("Time (min)")
+    plt.legend()
+    plt.grid()
+    plt.show()
 
-fig, ax = plt.subplots(5)
+    fig, ax = plt.subplots(5)
 
-ax[0].plot(Time, George.dataframe['BIS'])
-ax[1].plot(Time, George.dataframe['MAP'])
-ax[2].plot(Time, George.dataframe['CO'])
-ax[3].plot(Time, George.dataframe['TOL'])
-ax[4].plot(Time, George.dataframe['v_blood'])
+    ax[0].plot(Time, George.dataframe['BIS'])
+    ax[1].plot(Time, George.dataframe['MAP'])
+    ax[2].plot(Time, George.dataframe['CO'])
+    ax[3].plot(Time, George.dataframe['TOL'])
+    ax[4].plot(Time, George.dataframe['v_blood'])
 
-ax[0].set_ylabel("BIS")
-ax[1].set_ylabel("MAP")
-ax[2].set_ylabel("CO")
-ax[3].set_ylabel("TOL")
-ax[4].set_ylabel("blood volume")
-ax[4].set_xlabel("Time (min)")
-for i in range(5):
-    ax[i].grid()
-plt.ticklabel_format(style='plain')
-plt.show()
+    ax[0].set_ylabel("BIS")
+    ax[1].set_ylabel("MAP")
+    ax[2].set_ylabel("CO")
+    ax[3].set_ylabel("TOL")
+    ax[4].set_ylabel("blood volume")
+    ax[4].set_xlabel("Time (min)")
+    for i in range(5):
+        ax[i].grid()
+    plt.ticklabel_format(style='plain')
+    plt.show()
+
+# %% test
+index_end_bleeding = int(12*60/ts)
+index_end_transfusion = int(56*60/ts)
+
+
+# if bleeding is not stopped, BIS, MAP and CO should decrease, and TOL should increase
+assert George.dataframe['BIS'][0] > George.dataframe['BIS'][index_end_bleeding]
+assert George.dataframe['MAP'][0] > George.dataframe['MAP'][index_end_bleeding]
+assert George.dataframe['CO'][0] > George.dataframe['CO'][index_end_bleeding]
+assert George.dataframe['TOL'][0] < George.dataframe['TOL'][index_end_bleeding]
+
+# if transfusion is not stopped, BIS, MAP and CO should increase, and TOL should decrease
+assert George.dataframe['BIS'][index_end_bleeding] < George.dataframe['BIS'][index_end_transfusion]
+assert George.dataframe['MAP'][index_end_bleeding] < George.dataframe['MAP'][index_end_transfusion]
+assert George.dataframe['CO'][index_end_bleeding] < George.dataframe['CO'][index_end_transfusion]
+assert George.dataframe['TOL'][index_end_bleeding] > George.dataframe['TOL'][index_end_transfusion]
+
+
+print('test ok')
--- tests/test_equilibrium.py
+++ tests/test_equilibrium.py
@@ -11,60 +11,79 @@ import matplotlib.pyplot as plt
 from src.python_anesthesia_simulator import simulator
 
 
-Ts = 60
+Ts = 120
 George_1 = simulator.Patient([18, 170, 60, 0], ts=Ts, model_propo="Eleveld", model_remi="Eleveld", co_update=True)
 George_1.tol_pd.plot_surface()
 
 # %% Simulation
 
-N_simu = int(360 * 60/Ts)
+N_simu = int(720 * 60/Ts)
 
+bis_target_1 = 50
+tol_target_1 = 0.9
+map_target_1 = 80
 George_1.initialized_at_maintenance(bis_target=50, tol_target=0.9, map_target=80)
 uP, uR, uN = George_1.u_propo_eq, George_1.u_remi_eq, George_1.u_nore_eq
 Bis, Co, Map, Tol = George_1.one_step(u_propo=uP, u_remi=uR, u_nore=uN, noise=False)
+
+bis_target_2 = 40
+tol_target_2 = 0.95
+map_target_2 = 85
 uP, uR, uN = George_1.find_equilibrium(bis_target=40, tol_target=0.95, map_target=85)
 for index in range(N_simu):
     Bis, Co, Map, Tol = George_1.one_step(u_propo=uP, u_remi=uR, u_nore=uN, noise=False)
 
+# %% plot
+if __name__ == '__main__':
+    fig, ax = plt.subplots(3)
+    Time = George_1.dataframe['Time']/60
+    ax[0].plot(Time, George_1.dataframe['u_propo'])
+    ax[1].plot(Time, George_1.dataframe['u_remi'])
+    ax[2].plot(Time, George_1.dataframe['u_nore'])
 
-fig, ax = plt.subplots(3)
-Time = George_1.dataframe['Time']/60
-ax[0].plot(Time, George_1.dataframe['u_propo'])
-ax[1].plot(Time, George_1.dataframe['u_remi'])
-ax[2].plot(Time, George_1.dataframe['u_nore'])
+    ax[0].set_ylabel("Propo")
+    ax[1].set_ylabel("Remi")
+    ax[2].set_ylabel("Nore")
+    for i in range(3):
+        ax[i].grid()
 
-ax[0].set_ylabel("Propo")
-ax[1].set_ylabel("Remi")
-ax[2].set_ylabel("Nore")
-for i in range(3):
-    ax[i].grid()
+    plt.show()
 
-plt.show()
+    fig, ax = plt.subplots(1)
 
-fig, ax = plt.subplots(1)
+    ax.plot(Time, George_1.dataframe['x_propo_4'], label="Propofol")
+    ax.plot(Time, George_1.dataframe['x_remi_4'], label="Remifentanil")
+    ax.plot(Time, George_1.dataframe['c_blood_nore'], label="Norepinephrine")
+    plt.title("Hypnotic effect site Concentration")
+    ax.set_xlabel("Time (min)")
+    plt.legend()
+    plt.grid()
+    plt.show()
 
-ax.plot(Time, George_1.dataframe['x_propo_4'], label="Propofol")
-ax.plot(Time, George_1.dataframe['x_remi_4'], label="Remifentanil")
-ax.plot(Time, George_1.dataframe['c_blood_nore'], label="Norepinephrine")
-plt.title("Hypnotic effect site Concentration")
-ax.set_xlabel("Time (min)")
-plt.legend()
-plt.grid()
-plt.show()
+    fig, ax = plt.subplots(4)
 
-fig, ax = plt.subplots(4)
+    ax[0].plot(Time, George_1.dataframe['BIS'])
+    ax[1].plot(Time, George_1.dataframe['MAP'])
+    ax[2].plot(Time, George_1.dataframe['CO'])
+    ax[3].plot(Time, George_1.dataframe['TOL'])
 
-ax[0].plot(Time, George_1.dataframe['BIS'])
-ax[1].plot(Time, George_1.dataframe['MAP'])
-ax[2].plot(Time, George_1.dataframe['CO'])
-ax[3].plot(Time, George_1.dataframe['TOL'])
+    ax[0].set_ylabel("BIS")
+    ax[1].set_ylabel("MAP")
+    ax[2].set_ylabel("CO")
+    ax[3].set_ylabel("TOL")
+    ax[3].set_xlabel("Time (min)")
+    for i in range(4):
+        ax[i].grid()
+    plt.ticklabel_format(style='plain')
+    plt.show()
 
-ax[0].set_ylabel("BIS")
-ax[1].set_ylabel("MAP")
-ax[2].set_ylabel("CO")
-ax[3].set_ylabel("TOL")
-ax[3].set_xlabel("Time (min)")
-for i in range(4):
-    ax[i].grid()
-plt.ticklabel_format(style='plain')
-plt.show()
+# %% test
+# Verify that the equilibrium is reached at the beginning and at the end of the simulation
+assert abs(George_1.dataframe['BIS'].iloc[0]-bis_target_1) < 5e-1
+assert abs(George_1.dataframe['BIS'].iloc[-1]-bis_target_2) < 1
+assert abs(George_1.dataframe['TOL'].iloc[0]-tol_target_1) < 1e-2
+assert abs(George_1.dataframe['TOL'].iloc[-1]-tol_target_2) < 1e-2
+assert abs(George_1.dataframe['MAP'].iloc[0]-map_target_1) < 1e-2
+assert abs(George_1.dataframe['MAP'].iloc[-1]-map_target_2) < 1e-1
+
+print('test ok')
