34812dc483e7e1263faf0c44e5a317f17a30e663
Bob Aubouin <bob.aubouin@tutanota.com>
2023-04-28 14:19:09 +0200

    add models to pas import

--- src/python_anesthesia_simulator/__init__.py
+++ src/python_anesthesia_simulator/__init__.py
@@ -1,3 +1,5 @@
 from .simulator import Patient
 from .disturbances import compute_disturbances
 from .metrics import compute_control_metrics
+from .pk_models import CompartmentModel
+from .pd_models import BIS_model, TOL_model, Hemo_PD_model
